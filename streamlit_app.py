fe0b6c95173fa7781f1ae7a9f8bf473bef19401a
swathi10n <swathiniyer@gmail.com>
2024-12-18 00:28:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
-
-else:
-    st.warning("Please make sure both ingredients and name are provided.")
     
 #New session to display smoothiefroot nutrition information
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
