aed5fbdf56bcd622e13224080beef3be8c496526
swathi10n <swathiniyer@gmail.com>
2024-12-17 23:34:54 +0530

    Update streamlit_app.py
    Updated and improted response

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,8 @@ time_to_insert = st.button('Submit Order')
 if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
+
+#New session to display smoothiefroot nutrition information
+import requests
+smoothiefroot_response = response.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
