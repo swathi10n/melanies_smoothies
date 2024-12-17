f4e6b52ef8a9150277f37d947f79a0b9d2f5303e
swathi10n <swathiniyer@gmail.com>
2024-12-18 00:27:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,15 +33,15 @@ if ingredients_list:
 
     #st.write(ingredients_string)
 
-my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order) values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
+    my_insert_stmt = f""" insert into smoothies.public.orders(ingredients, name_on_order) values ('""" + ingredients_string + """', '""" + name_on_order + """')"""
 
-#st.write(my_insert_stmt)
+    #st.write(my_insert_stmt)
 
-time_to_insert = st.button('Submit Order')
+    time_to_insert = st.button('Submit Order')
     
-if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success(f"Your Smoothie is ordered, {name_on_order}!", icon="✅")
 
 else:
     st.warning("Please make sure both ingredients and name are provided.")
